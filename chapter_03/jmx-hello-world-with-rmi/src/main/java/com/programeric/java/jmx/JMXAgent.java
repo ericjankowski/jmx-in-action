c722dffe656f006ea93768e85ef64c6749cc72ea
Eric Jankowski <eric.jankowski@mozido.com>
2013-08-29 14:59:06 -0500

    manual jar stuff double sucks

--- chapter_03/jmx-hello-world-with-rmi/src/main/java/com/programeric/java/jmx/JMXAgent.java
+++ chapter_03/jmx-hello-world-with-rmi/src/main/java/com/programeric/java/jmx/JMXAgent.java
@@ -17,8 +17,22 @@ public class JMXAgent {
 		
 	}
 	
-	protected void startHTMLAdapter(){
+	private void startHTMLAdapter(){
 		HtmlAdaptorServer adapter = new HtmlAdaptorServer();
+		ObjectName adapterName = null;
+		
+		try{
+			adapter.setPort(9092);
+			adapterName = new ObjectName("JMXAgent:name=html,port=9092");
+			server.registerMBean(adapter, adapterName);
+			adapter.start();
+		}catch (Exception e){
+			System.out.println("Error Starting HTML Adapter for Agent");
+		}
+	}
+	
+	private void startRMIConnector(){
+		RmiConnectorServer connectorserver = new RmiConnectorServer();
 	}
 	
 	public static void main(String [] args){
