0352645eb09241fbb589d2aa1816150f10a30e13
jankenstein <eric.r.jankowski@gmail.com>
2013-08-30 13:12:28 -0500

    Chapter 3: got the HTML adapter up and running

--- chapter_03/jmx-hello-world-with-rmi/src/main/java/com/programeric/java/jmx/JMXAgent.java
+++ chapter_03/jmx-hello-world-with-rmi/src/main/java/com/programeric/java/jmx/JMXAgent.java
@@ -3,6 +3,9 @@ package com.programeric.java.jmx;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerFactory;
 import javax.management.ObjectName;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
 
 import com.sun.jdmk.comm.HtmlAdaptorServer;
 
@@ -17,12 +20,33 @@ public class JMXAgent {
 		
 	}
 	
-	protected void startHTMLAdapter(){
+	private void startHTMLAdapter(){
 		HtmlAdaptorServer adapter = new HtmlAdaptorServer();
+		ObjectName adapterName = null;
+		
+		try{
+			adapter.setPort(9092);
+			adapterName = new ObjectName("JMXAgent:name=html,port=9092");
+			server.registerMBean(adapter, adapterName);
+			adapter.start();
+		}catch(Exception e){
+			System.err.println("Error Starting HTML Adapter for JMXAgent: " + e.getMessage());
+		}
+	}
+	
+	private void startRMIConnector(){
+		try{
+			JMXServiceURL jmxServiceUrl = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://localhost:2099/server");
+			JMXConnectorServer connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(jmxServiceUrl, null, server);
+			connectorServer.start();
+		}catch(Exception e){
+			System.err.println("Error Starting RMI Connector for JMXAgent: " + e.getMessage());
+		}
 	}
 	
 	public static void main(String [] args){
-		System.out.println("JMXAgent is running");
+		System.out.println("JMXAgent is running...");
 		JMXAgent agent = new JMXAgent();
+		
 	}
 }
